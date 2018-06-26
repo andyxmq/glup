cd41a55a35590c0336bcd850d225ecdea362dc19
andyxmq <1714903660@qq.com>
2018-06-26 23:32:16 +0800

    asdaad

--- webpack/src/common/components/DefaultLayout.jsx
+++ webpack/src/common/components/DefaultLayout.jsx
@@ -30,7 +30,7 @@ class DefaultLayout extends Component{
                             <Menu.Item key="1">option1</Menu.Item>
                             <Menu.Item key="2">option2</Menu.Item>
                             <Menu.Item key="3">option3</Menu.Item>
-                            <Menu.Item key="4">option4</Menu.Item>
+                            <Menu.Item key="4">optioasdasdn4</Menu.Item>
                         </SubMenu>
 
                     </Menu>
