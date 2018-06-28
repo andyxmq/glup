5a4001746729ed78cb97a795c485e8ca48b5cfd1
xumq <XUMQ@glodon.com>
2018-06-28 18:51:19 +0800

    'asd'

--- webpack/src/js/show.js
+++
@@ -1,5 +0,0 @@
-// var str = require('./str');
-let fnstr = require('./str');
-// require('!style-loader!css-loader!./style.css');
-require('../css/style.css');
-document.write(fnstr('<div>天气非常好！</div>'));
--- webpack/src/js/str.js
+++
@@ -1,7 +0,0 @@
-// var json = {
-//     str: '后天是一个大晴天',
-// }
-function outStr(str){
-    return str;
-}
-module.exports = outStr;
